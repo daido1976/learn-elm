39f323eb0caff4c3ce9e228a21c4d12760d7063c
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-08-11 14:44:32 +0900

    Refactor Useful Links with variable assignment

--- src/Main.elm
+++ src/Main.elm
@@ -7,10 +7,18 @@ import Html.Attributes exposing (href)
 main : Html msg
 main =
     div []
-        [ h1 [] [ text "Useful Links" ]
-        , ul []
-            [ li [] [ a [ href "https://elm-lang.org/" ] [ text "Top" ] ]
-            , li [] [ a [ href "https://package.elm-lang.org/" ] [ text "Packages" ] ]
-            , li [] [ a [ href "https://elm-lang.org/docs" ] [ text "Docs" ] ]
-            ]
+        [ header, content ]
+
+
+header : Html msg
+header =
+    h1 [] [ text "Useful Links" ]
+
+
+content : Html msg
+content =
+    ul []
+        [ li [] [ a [ href "https://elm-lang.org/" ] [ text "Top" ] ]
+        , li [] [ a [ href "https://package.elm-lang.org/" ] [ text "Packages" ] ]
+        , li [] [ a [ href "https://elm-lang.org/docs" ] [ text "Docs" ] ]
         ]
