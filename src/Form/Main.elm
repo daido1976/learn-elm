65e3a3df5d58a5bc40ba64a4785089e3d0b78f6c
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-08-11 17:46:16 +0900

    Implement Form

--- src/Form/Main.elm
+++ src/Form/Main.elm
@@ -20,12 +20,12 @@ main =
 
 
 type alias Model =
-    {}
+    { input : String, memos : List String }
 
 
 init : Model
 init =
-    {}
+    { input = "", memos = [] }
 
 
 
@@ -33,12 +33,18 @@ init =
 
 
 type Msg
-    = Msg
+    = Input String
+    | Submit
 
 
 update : Msg -> Model -> Model
 update msg model =
-    model
+    case msg of
+        Input input ->
+            { model | input = input }
+
+        Submit ->
+            { model | input = "", memos = model.input :: model.memos }
 
 
 
@@ -47,4 +53,15 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    text ""
+    div []
+        [ Html.form [ onSubmit Submit ]
+            [ input [ value model.input, onInput Input ] []
+            , button [ disabled (String.length model.input < 1) ] [ text "Submit!" ]
+            ]
+        , ul [] (List.map viewMemo model.memos)
+        ]
+
+
+viewMemo : String -> Html Msg
+viewMemo memo =
+    li [] [ text memo ]
