bf2fb6a518b89bd3e1e3d2ca9006ecf92a76f74f
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-08-12 12:51:56 +0900

    Implement Http

---
+++ src/Http/Main.elm
@@ -0,0 +1,68 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (..)
+import Html.Events exposing (..)
+import Http
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { result : String }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { result = "" }, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Click
+    | GotRepo (Result Http.Error String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Click ->
+            ( model
+            , Http.get
+                { url = "https://api.github.com/repos/daido1976/elm-basics"
+                , expect = Http.expectString GotRepo
+                }
+            )
+
+        GotRepo (Ok repo) ->
+            ( { model | result = repo }, Cmd.none )
+
+        GotRepo (Err error) ->
+            ( { model | result = Debug.toString error }, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Click ] [ text "Get Repo Info" ]
+        , p [] [ text model.result ]
+        ]
